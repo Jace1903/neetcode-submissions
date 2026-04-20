f07ffaace242ebe96e449f0b26a428fc757efb73
Jace Richard Lopes <86949238+Jace1903@users.noreply.github.com>
2026-04-19 18:23:00 -0700

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        hashmap={}
+        for num in nums:
+            if num in hashmap:
+                hashmap[num]+=1
+            else:
+                hashmap[num]=1
+            if hashmap[num]>1:
+                return True
+            
+        return False
+
+    
+            
+        
